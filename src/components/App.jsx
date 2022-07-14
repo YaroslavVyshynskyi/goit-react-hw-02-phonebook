ce92fee8c42cf9debbc1a3bc93320dcce2e1e7f8
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-07-14 23:03:03 +0300

    create components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
+import { Component } from "react";
+import ContactForm from "componentsContactForm/ContactForm"
+
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: ''
+  };
+  
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ContactForm />
+      phonebook
     </div>
   );
+  
 };
+
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types"
+
+
+const ContactForm = () => {
+    return (
+        // <p className="form_title">Phonebook</p>
+        
+        <div className="form_container">
+            
+        </div>
+    )
+};
+
+export default ContactForm;
